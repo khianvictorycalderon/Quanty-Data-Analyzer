f7bbb045461d21a9f3e6d855dd391b732bec841f
khian calderon <ckhianvictory@gmail.com>
2025-09-09 21:12:25 +0800

    Updated location

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero";
 import NavBar from "./components/navbar";
 import OpSec from "./components/opsec";
 import Privacy_Terms from "./components/privac&terms";
-import { slideToID } from "./Utility";
+import { slideToID } from "./utility";
 
 export default function App() {
 
